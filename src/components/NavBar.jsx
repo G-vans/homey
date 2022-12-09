2328abc0317268d0b7c98f6f4848550f4f947127
G-vans <jevans.otieno@student.moringaschool.com>
2022-12-09 16:45:22 +0300

    feat:create navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import { Link, Outlet } from "react-router-dom"
+
+export default function NavBar({ user, setUser }) {
+
+
+  function handleLogoutClick() {
+    fetch("http://localhost:3000/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    });
+  }
+  
+  
+  return (
+    <div className='header'>
+      <div className='navbar-container'>
+        <Link className="navbar-logo" to="/">Estate-Intel</Link> <div className='nav-menu'>
+          <Link className='nav-links' to="/">Home</Link>
+          <Link className='nav-links' to="/properties">Property</Link>
+          
+          {user === null  ? <Link className='nav-links' to='/auth'>Login</Link> : <Link className='nav-links' onClick={handleLogoutClick}>Logout</Link>}
+        </div>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
